bf8603ad1f612c64aff30e70d388973f86e862e3
Hongmin Kim <hongmin010@naver.com>
2022-04-14 23:20:13 +0900

    edited gugudan

--- gugudan.cpp
+++ gugudan.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 8; j++)
+        for (int j = 1; j <= 9; j++)
             cout << j << " x " << i << " = " << j * i << "\t";
         cout << endl;
     }
